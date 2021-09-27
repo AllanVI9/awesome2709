5eba2809fd85dbe8b502b2560ae243fa58cba167
Allan <61694786+AllanVI9@users.noreply.github.com>
2021-09-27 12:41:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//servidor 
+var express = require("express")
+var app = express()
+//config database com acesso ao mongoDBatlas
+var mongoose = require('mongoose')
+var conexao = ()=>{
+    var caminho = mongoose.connect('mongodb+srv://somenteleitura:ivaSomenteleitura9@fiap.9lwql.mongodb.net/mongoatlas?retryWrites=true&w=majority')
+    }
+var schema = mongoose.Schema
+var depoimentos = new schema({
+    nome:String,
+    cargo:String,
+    mensagem:String
+})
+var documentos = mongoose.model('depoimentos',depoimentos)
+//fim das configurações do database
+var porta = process.env.PORT || 4040
+// configurações 
+app.set("view engine",'ejs')
+app.use(express.static('./'))
+//rota para abrir o arquivo index.ejs
+app.get('/',(req,res)=>{
+    conexao()
+    documentos.find().limit(3).sort({'_id':-1})
+    .then((documentos)=>{
+        console.log(documentos)
+        res.render('index',{documentos})
+    })
+    })
+//ligar o servidor 
+app.listen(porta)
+  
